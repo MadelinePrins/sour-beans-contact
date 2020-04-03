695aef24621e983e92aca19e18f929f862c4ed8f
Madeline Prins <mprins@MacBook-Pro-2.local>
2020-04-02 22:43:05 -0700

    initial outline. pre-functions. april 2

---
+++ Javascript-Functions/hideElements.js
@@ -0,0 +1,11 @@
+
+//JavaScript function that hides elements until revealed by a button 
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
